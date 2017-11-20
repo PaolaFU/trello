2aa008e4d76b33c705655aeb0855db296aac54cb
PaolaFU <paola_sfu@hotmail.com>
2017-11-20 04:46:46 -0500

    Subiendo version 0.0.1

--- js/app.js
+++ js/app.js
@@ -1,45 +1,20 @@
 window.addEventListener('load', function() {
   var container = document.getElementById('container');
-  var box = document.getElementById('box');
   var form = document.getElementById('form');
-  var tarea = document.getElementById('tarea');
+  var lista = document.getElementById('lista');
   var btnSave = document.getElementById('btn-save');
+  var newlist = document.getElementById('newlist');
 
- 
-
-  function añadirLista(event){
-    e.preventDefault();
-    box.style.display = "inline-block";
-    span.style.display = "none";
-    form.style.display = "inline-block";
-    texto.focus();
-    }
-  function addTarea(event) {
-    form.style.display = 'none';
-    var form = document.createElement('form');
-    var textArea = document.createElement('textArea');
-    textArea.classList.add('texto');
-    
-    var newButton = document.createElement('newButton');
-    var button = document.createTextNode('Agregar');
-    newButton.classList.add('newButton');
-    form.appendChild(textArea);
-    form.appendChild(newButton);
-    textArea.focus();
-  }
-
-  function crearLista() {
-    var lista = document.createElement('div');
-    lista.textContent = texto.value;
-    lista.classList.add('lista');
-    contenedor.insertBefore(lista, span.previousSibling);
-    texto.value = '';
-    var nuevaTarjeta = document.createElement('a');
-    nuevaTarjeta.href = '#';
-    var nodoTarjeta = document.createTextNode('Añadir una tarjeta ...');
-    nuevaTarjeta.appendChild(nodoTarjeta);
-    lista.appendChild(nuevaTarjeta);
-    nuevaTarjeta.classList.add('tarjeta');
-    nuevaTarjeta.addEventListener('click', añadirNuevaTarjeta);
+  // Al hacer click en 'añadir lista' 
+  newlist.addEventListener('click', function(event) {
+    event.preventDefault();
+    otherContainer();
+  });
+  // funcion que oculta 'añadir lista' y aparece el container
+  function otherContainer() {
+    lista.style.cssText = 'display:block';
+    btnSave.style.cssText = 'display:block';
+    newlist.style.cssText = 'display:none';
+    container.style.cssText = 'display:block';
   }
 });
